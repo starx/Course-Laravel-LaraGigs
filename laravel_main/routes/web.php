ad65f286b8a234244d8d548454a98477468fb8ba
starx <mail@mrnepal.com>
2022-08-13 10:18:23 +0100

    Made previous route more streamlined. Added route that demonstrate: Accepting custom parameter and constrained parameters.

--- laravel_main/routes/web.php
+++ laravel_main/routes/web.php
@@ -18,16 +18,31 @@ Route::get('/', function () {
 });
 
 Route::get('/hello', function () {
-    return response('<h1>Hello world</h1>', 200)
-        ->header('Content-Type', 'text/html');
+    return response('<h1>Hello world</h1>');
+});
+
+Route::get('/hello/custom-status-code', function () {
+    return response('<h1>Hello world</h1>', 202);
 });
 
 Route::get('/hello/plain', function () {
-    return response('<h1>Hello world</h1>', 200)
+    return response('<h1>Hello world</h1>')
         ->header('Content-Type', 'text/plain');
 });
 
 Route::get('/hello/custom-header-value', function () {
-    return response('<h1>Hello world</h1>', 200)
+    return response('<h1>Hello world</h1>')
         ->header('foo', 'bar');
 });
+
+Route::get('/hello/{subject}', function ($subject) {
+    return response('<h1>Hello '. $subject . '</h1>');
+});
+
+/*
+ *  Constraint route parameters
+ */
+Route::get('/hello/number/{id}', function ($id) {
+    return response('<h1>Hello number '. $id . '</h1>');
+})
+->where('id', '[0-9]+');
